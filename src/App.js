99cf24925d0950842e5d6471b5889562e525fdc3
Eirik <eah001@student.uib.no>
2019-03-04 14:14:48 +0100

    Changing userlist from json file to placeholder api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import CardList from './CardList';
-import { robots } from './Robots';
 import SearchBox from './SearchBox';
 import './App.css';
 
@@ -8,26 +7,32 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     };
   }
 
-onSearchChange = (event) => {
-  this.setState({ searchfield: event.target.value })
-}
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json()).then(users => this.setState({robots: users}));
+  }
+
+  onSearchChange = (event) => {
+    this.setState({searchfield: event.target.value})
+  }
 
   render() {
-    const filteredRobots = this.state.robots.filter(robot =>{
+    const filteredRobots = this.state.robots.filter(robot => {
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
     })
-    return (
-      <div className ='tc'>
-        <h1 className ='f1'>RoboFriends</h1>
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>
+    } else {
+      return (<div className='tc'>
+        <h1 className='f1'>RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange}/>
         <CardList robots={filteredRobots}/>
-      </div>
-    );
+      </div>);
+    }
   }
 }
 
