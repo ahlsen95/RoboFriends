de1ea1e1128e2fcb6cca970d18cb342421a758cf
Eirik <eah001@student.uib.no>
2019-06-03 14:54:21 +0200

    Updates

---
+++ precache-manifest.9cf19f9b652eb6321e7fc6a5901577dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "6a05eb6cb7d034d3ccb0",
+    "url": "/RoboFriends/static/js/runtime~main.6a05eb6c.js"
+  },
+  {
+    "revision": "fc507b7f21a09726bd67",
+    "url": "/RoboFriends/static/js/main.fc507b7f.chunk.js"
+  },
+  {
+    "revision": "97442ae5fa17bd9866b6",
+    "url": "/RoboFriends/static/js/2.97442ae5.chunk.js"
+  },
+  {
+    "revision": "fc507b7f21a09726bd67",
+    "url": "/RoboFriends/static/css/main.9db35fab.chunk.css"
+  },
+  {
+    "revision": "97442ae5fa17bd9866b6",
+    "url": "/RoboFriends/static/css/2.0a5f2ff8.chunk.css"
+  },
+  {
+    "revision": "e6847a0bf700ac7a13e79a54ebe22ce6",
+    "url": "/RoboFriends/index.html"
+  }
+];
